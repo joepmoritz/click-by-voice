71d201ab3e563b97f70f4adf9084d70b8597bca7
Joep Moritz <joep.moritz@gmail.com>
2018-06-13 08:53:04 +0100

    Google inbox hints for attachments are now always placed as overlay in top left corner. Added hints for collapsed messages within a thread.

--- src/GoogleInbox.js
+++ src/GoogleInbox.js
@@ -8,15 +8,28 @@ var GoogleInbox = null;
 
 	function get_root_elements() {
 		// main message text block: div.b5
-		// attachments on collapsed message header: div.nJ.u2
-		return $('div\.b5, div\.nJ');
+		return $('div.b5');
 	}
 
 	function place_hints() {
-		// normal message header: b9
+		// normal thread header: an.b9
 		// group header: an.aT
 		// reminder header: an.cF
-		$('div\.an\.b9, div\.an\.aT, div\.an\.cF').each((index, element) => {
+		// attachments on collapsed thread header: div.nJ
+		// attachment below message body: div.u2.k9
+		// message header within a thread: j3.s2
+		// collapsed message headers (with number) within a thread: n0
+		overlay_before_hints = [
+			'an.b9', // normal thread header
+			'an.aT', // group header
+			'an.cF', // reminder header
+			'nJ.u2', // attachments on collapsed thread header
+			'nJ.s0', // Google maps attachment on the collapsed thread header
+			'u2.k9', // attachment below message body
+			'j3.s2', // message header within a thread
+			'n0', // collapsed message headers (with number) within thread
+		];
+		$('div.' + overlay_before_hints.join(',div.')).each((index, element) => {
 			Hints.add_hint($(element), (e, nr) => AddHint.add_overlay_hint(e, nr, false));
 		});
 	}
